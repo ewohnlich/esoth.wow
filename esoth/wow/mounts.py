c0f76fa3e95661eee4b6b44694f775e6dd0cf6d5
Eric Wohnlich <oolitegroove@gmail.com>
2013-08-01 20:50:25 -0400

    best mount data for awhile probably

--- esoth/wow/mounts.py
+++ esoth/wow/mounts.py
@@ -25,8 +25,14 @@ class MountUtility:
     """
     data = json.load(open(os.path.join(os.path.dirname(__file__),'mounts.json')))
     _mounts = []
+    _flag = False
     for id,info in data.items():
-      _mount = {'icon':'','isCollected':False,'isGround':False,'isFlying':False,'isAquatic':False,'isJumping':False,
+      _mount = {'icon':info.get('icon'),
+                'isCollected':info.get('isCollected'),
+                'isGround':info.get('isGround'),
+                'isFlying':info.get('isFlying'),
+                'isAquatic':info.get('isAquatic'),
+                'isJumping':info.get('isJumping'),
                 'spellId':id,
                 'name':info['name'],
                 'location':info['location'],
@@ -37,16 +43,12 @@ class MountUtility:
         
       if id in mounts:
         if mounts[id]['name'] != info['name']:
-          import pdb; pdb.set_trace()
           logger.warn('%s (import) vs %s (blizz)' % (info['name'],mounts[id]['name']))
         if info['location'] == 'unknown':
-          import pdb; pdb.set_trace()
           logger.warn('unknown location - %s' % info['name'])
         if info['faction'] == 'U':
-          import pdb; pdb.set_trace()
           logger.warn('unknown faction - %s' % info['name'])
         if info['obtainable'] == 'U':
-          import pdb; pdb.set_trace()
           logger.warn('unknown if obtainable - %s' % info['obtainable'])
         _mount.update({'isCollected':True,
                        'icon':mounts[id]['icon'],
@@ -56,27 +58,34 @@ class MountUtility:
                        'isFlying':mounts[id]['isFlying'],
                        'isAquatic':mounts[id]['isAquatic'],
                        'isJumping':mounts[id]['isJumping']})
+        if not data[id].get('icon'):
+          data[id]['icon'] = mounts[id]['icon']
+          data[id]['isGround'] = mounts[id]['isGround']
+          data[id]['isFlying'] = mounts[id]['isFlying']
+          data[id]['isAquatic'] = mounts[id]['isAquatic']
+          data[id]['isJumping'] = mounts[id]['isJumping']
+          logger.info('added icon and statuses for %s' % str(id))
+          _flag = True
+        
       _mounts.append(self.applyClasses(_mount,default_faction))
-    _flag = False
     for id in mounts.keys():
       # we don't have any data on this one
       if id not in data:
         _flag = True
-        import pdb; pdb.set_trace()
         logger.warn('added - %s' % info['name'])
-#        data[id] = {'name':mounts[id]['name'],'faction':'U','obtainable':'U','location':'unknown'}
-#        _mounts.append(self.applyClasses({'isCollected':True,
-#                        'icon':mounts[id]['icon'],
-#                        'location':'unknown',
-#                        'faction':'U',
-#                        'obtainable':'U',
-#                        'spellId':id,
-#                        'itemId':mounts[id]['itemId'],
-#                        'name':mounts[id]['name'],
-#                        'isGround':mounts[id]['isGround'],
-#                        'isFlying':mounts[id]['isFlying'],
-#                        'isAquatic':mounts[id]['isAquatic'],
-#                        'isJumping':mounts[id]['isJumping']},default_faction))
+        data[id] = {'name':mounts[id]['name'],'faction':'U','obtainable':'U','location':'unknown'}
+        _mounts.append(self.applyClasses({'isCollected':True,
+                        'icon':mounts[id]['icon'],
+                        'location':'unknown',
+                        'faction':'U',
+                        'obtainable':'U',
+                        'spellId':id,
+                        'itemId':mounts[id]['itemId'],
+                        'name':mounts[id]['name'],
+                        'isGround':mounts[id]['isGround'],
+                        'isFlying':mounts[id]['isFlying'],
+                        'isAquatic':mounts[id]['isAquatic'],
+                        'isJumping':mounts[id]['isJumping']},default_faction))
                    
     if _flag:
       json.dump(data,open(os.path.join(os.path.dirname(__file__),'mounts.json'),'w'))       
